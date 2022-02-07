fb02e639f6248de01f97c01d12acbae8f8f15dbf
Meet Madhu <meet.madhu@MBProC-MeetM.hitronhub.home>
2022-02-06 18:34:20 -0800

    form handle and display data

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Chart.css'
+
+import ChartBar from "./ChartBar";
+
+const Chart = () => {
+  <div className='chart'>
+    <ChartBar />
+  </div> 
+};
+
+export default Chart;
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartBar = () => {
+  return <div className="chart-bar"></div>;
+};
+
+export default ChartBar;
